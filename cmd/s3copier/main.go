378e91a0c160e11243740096b3ef3ea7718db6e6
Vianel Rodríguez <vianel.rodriguez@full360.com>
2018-10-16 17:06:01 -0500

    added more log

--- cmd/s3copier/main.go
+++ cmd/s3copier/main.go
@@ -12,18 +12,23 @@ import (
 )
 
 func requestHandler(S3Event events.S3Event) error {
-
 	targetBucket := os.Getenv("TARGET_S3_BUCKET")
 	grantFullControl := os.Getenv("OWNERSHIP_FULL_CONTROL")
 
-	s3, _ := awsF360.NewS3Client()
+	s3, err := awsF360.NewS3Client()
+	if err != nil {
+		log.Fatal("Error Getting S3 Client: ", err)
+	}
 
 	for _, rec := range S3Event.Records {
 		_, err := s3.PutObjectACL(rec.S3.Bucket.Name, rec.S3.Object.Key, grantFullControl)
 		if err != nil {
 			log.Fatal("Error Add ACL: ", err)
 		} else {
-			s3.CopyObjectToBucket(targetBucket, rec.S3.Bucket.Name, rec.S3.Object.Key)
+			_, errCP := s3.CopyObjectToBucket(targetBucket, rec.S3.Bucket.Name, rec.S3.Object.Key)
+			if errCP != nil {
+				log.Fatal("Error Copying file ", errCP)
+			}
 		}
 	}
 
